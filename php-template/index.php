fc837bdf5813e72861f9c17c3c33d6879ba2692e
SMcGregor199 <shaynemcgregor1@gmail.com>
2021-12-13 09:44:38 -0500

    add initial files for php-template project

---
+++ php-template/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>[[enter title]]</title>
+			<meta name="description" content="[[insert description]]">
+			<meta property="og:image" content="images/default-meta.png">
+
+			<link rel="stylesheet" href="css/style.css">
+
+			<style>
+				p {
+					color: red;
+					margin-top: 10px;
+					}
+			</style>
+
+		</head>
+
+		<body>
+			<?php 
+				$noun="shoe box";
+				$pNoun="buttons";
+				$pVerb="drive";
+				$bodyPart="arm";
+				$adjective="blue";
+
+				echo "<p> Today, every student has a computer small enough to fit into their " . $noun . ". You can solve any math problem by simply pushing the computer’s little " . $pNoun . ".</p>"
+			?>
+			<p>Computers can add, multiply, divide, and <?php echo $pVerb ?>. They can also <?php echo $pVerb ?> better than a human.</p>
+
+			<p>Some computers have their own <?=$bodyPart?>. Other’s have a <?=$adjective?> screen that shows all kinds of <?=$pNoun?> and <?=$adjective?> figures.</p>
+		</body>
+	</html>
