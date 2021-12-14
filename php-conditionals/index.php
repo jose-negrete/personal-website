d086c2b99f19a5ebc30a47ce59c4b49b519195c7
SMcGregor199 <shaynemcgregor1@gmail.com>
2021-12-14 15:25:09 -0500

    add initial files

---
+++ php-conditionals/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>[[enter title]]</title>
+			<meta name="description" content="[[insert description]]">
+			<meta property="og:image" content="images/default-meta.png">
+
+			<link rel="stylesheet" href="css/style.css">
+		</head>
+
+		<body>
+			<?php
+				$num1=15;
+				$num2=20;
+
+				if ($num2-$num1<7){
+					echo "The difference is less than 7"
+				}
+
+
+			?>
+		</body>
+	</html>
