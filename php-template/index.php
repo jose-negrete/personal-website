06d240322645f010e318f426afebf669a0b3dfa9
SMcGregor199 <shaynemcgregor1@gmail.com>
2021-12-16 06:09:58 -0500

    add list to php template

--- php-template/index.php
+++ php-template/index.php
@@ -10,30 +10,33 @@
 
 			<link rel="stylesheet" href="css/style.css">
 
-			<style>
-				body {
-					background-color: black;
-				}
-				p {
-					color: red;
-					margin-top: 10px;
-					}
-			</style>
 
 		</head>
 
 		<body>
 			<?php 
-				$noun="shoe box";
-				$pNoun="buttons";
-				$pVerb="drive";
-				$bodyPart="arm";
-				$adjective="blue";
+				$myFavoriteThings = ["bell peppers" , "comics" , "knick-knacks" , "the color blue"];
 
-				echo "<p> Today, every student has a computer small enough to fit into their " . $noun . ". You can solve any math problem by simply pushing the computer’s little " . $pNoun . ".</p>"
+				echo $myFavoriteThings[3];
+
+				echo "<p> I like the " . $myFavoriteThings[3] . "</p>";
+
+
+				$shayne = [
+					"age" => 29,
+					"favorite-food" => "soup",
+					"alive" => true,
+					"hometown" => "New York",
+					"favorite-place" => "home",
+
+				];
+
+				echo $shayne["favorite-place"];
 			?>
-			<p>Computers can add, multiply, divide, and <?php echo $pVerb ?>. They can also <?php echo $pVerb ?> better than a human.</p>
 
-			<p>Some computers have their own <?=$bodyPart?>. Other’s have a <?=$adjective?> screen that shows all kinds of <?=$pNoun?> and <?=$adjective?> figures.</p>
+			<p>My favorite food is <?php echo $shayne["favorite-food"];?></p>
+
+			<p>My favorite place is <?=$shayne["favorite-place"]?></p>
+		
 		</body>
 	</html>
