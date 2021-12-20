b3e8f3deacec98e5820296a79919ffd91fd88e9b
SMcGregor199 <shaynemcgregor1@gmail.com>
2021-12-20 10:33:36 -0500

    create list of rangers using php

--- php/php-power-rangers/index.php
+++ php/php-power-rangers/index.php
@@ -4,7 +4,7 @@
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 
-			<title>[[enter title]]</title>
+			<title>The Mighty Morphing PHP Rangers</title>
 			<meta name="description" content="[[insert description]]">
 			<meta property="og:image" content="images/default-meta.png">
 
@@ -13,57 +13,67 @@
 
 		<body>
 
-			<h1>The Mighty Morphing Power Rangers</h1>
+			<h1>The Mighty Morphing PHP Rangers</h1>
 
 			<?php
 				
 				$Jason = [
 
+					"name" => "Jason",
 					"age" => 16,
 					"ranger-color" => "red",
 					"home-town" => "Angel Grove",
 					"zord-name" => "Tyrannosaurus Dinozord",
 					"can-morph" => true,
+					"picture" => "'images/jason.jpg'"
 
 				];
 
 				$Kimberly = [
 
+					"name" => "Kimberly",
 					"age" => 16,
 					"ranger-color" => "pink",
 					"home-town" => "Angel Grove",
 					"zord-name" => "Pterodactyl Dinozord",
 					"can-morph" => true,
+					"picture" => "'images/kimberly.jpg'"
 
 				];
 
-				$Zach = [
+				$Zack = [
 
+					"name" => "Zack",
 					"age" => 16,
 					"ranger-color" => "black",
 					"home-town" => "Angel Grove",
 					"zord-name" => "Mastodon Dinozord",
 					"can-morph" => true,
+					"picture" => "'images/zack.jpg'"
 
 				];
 
 				$Trini = [
 
+					"name" => "Trini",
 					"age" => 16,
 					"ranger-color" => "yellow",
 					"home-town" => "Angel Grove",
 					"zord-name" => "Saber-Toothed Tiger Dinozord",
 					"can-morph" => true,
+					"picture" => "'images/trini.jpg'"
 
 				];
 
 				$Billy = [
 
+					"name" => "Billy",
 					"age" => 16,
 					"ranger-color" => "blue",
 					"home-town" => "Angel Grove",
 					"zord-name" => "Triceratops Dinozord",
 					"can-morph" => true,
+					"picture" => "'images/billy.jpg'",
 
 				];
 
@@ -71,20 +81,56 @@
 					"under-attack" => true,
 				];
 
-				$mmpRangers = [$Jason, $Kimberly, $Zach, $Trini, $Billy];
+				function morphingGrid($name, $age, $color, $homeTown, $zordName, $canMorph, $picture) {
+					
+					$ranger = [
+						"name" => $name,
+						"age" => $age,
+						"ranger-color" => $color,
+						"home-town" => $homeTown,
+						"zord-name" => $zordName,
+						"can-morph" => $canMorph,
+						"picture" => $picture,
 
+					];
 
-				foreach ($mmpRangers as $ranger) {
-
-						echo $ranger["zord-name"];
+					return $ranger; //this turns the function in to the object that it creates. 
 				}
 
-				if($angleGrove["under-attack"] && $Jason["can-morph"]) {
-					echo "<p> and Jason yelled 'it's Morphing Time' </p>";
-				} 
+				$Tommy = morphingGrid("Tommy", 17, "green", "unknown", "Dragon Zord", true, "images/tommy.jpg");
+
+				// echo $Tommy[name];
+
+				// echo "<img src=" . $Tommy["picture"] . ">";
+
+
+
+				$mmpRangers = [$Jason, $Kimberly, $Zack, $Trini, $Billy, $Tommy];
+
+				foreach($mmpRangers as $mmpRanger) {
+					echo "<picture class='ranger-image'>";
+						echo "<img src=" . $mmpRanger["picture"] . ">";
+					echo "</picture>";
+
+					echo "<p>This ranger's name is " . $mmpRanger["name"] . "</p>";
+					echo "<p>They are the " . $mmpRanger["ranger-color"] . " ranger</p>";
 				
 
+				}
+
+
+
+				// if($angleGrove["under-attack"] && $Jason["can-morph"]) {
+				// 	echo "<p> and Jason yelled 'it's Morphing Time' </p>";
+				// } 
+				
+			
 			?>
+
+			<picture class="ranger-image">
+				<img src="">
+			</picture>
+			
 
 			
 		</body>
