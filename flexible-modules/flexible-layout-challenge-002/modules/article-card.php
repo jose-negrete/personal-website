651788e1102b5852d889f4fdd4681257af33b563
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-01-11 09:33:46 -0500

    edits to module website

---
+++ flexible-modules/flexible-layout-challenge-002/modules/article-card.php
@@ -0,0 +1,7 @@
+				<div class="article-card">
+					<h1>This is the title of an article card</h1>
+
+					<p class="general-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+					<a href="">READ MORE</a>
+				</div>
---
+++ flexible-modules/flexible-layout-challenge-002/modules/call-to-action.php
@@ -0,0 +1,7 @@
+<call-to-action>
+	<h1>This module is a "call to action"</h1>
+
+	<p class="general-voice">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>						
+
+	<a href="">HERE'S THE ACTION</a>
+</call-to-action>
