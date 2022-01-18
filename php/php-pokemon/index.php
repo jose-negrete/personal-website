1f4bbfded399aacb200a7d931a53060f5ec00e8b
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-01-18 18:17:40 -0500

    create a list of pokemon

---
+++ php/php-pokemon/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>The Mighty Morphing PHP Rangers</title>
+			<meta name="description" content="[[insert description]]">
+			<meta property="og:image" content="images/default-meta.png">
+
+			<link rel="stylesheet" href="css/style.css">
+
+			<style>
+				body {
+					color: blue;
+					font-size: 24px;
+				}
+
+				ol {
+					list-style:  decimal;
+					padding: 50px;
+				}
+			</style>
+		</head>
+
+		<body>
+
+		<?php 
+		 $pokemonData = file_get_contents("monsters.json");
+
+		 $pokemon = json_decode($pokemonData, true);
+
+		?>
+		
+		<ol>
+			<?php foreach($pokemon as $poke) { ?>
+				<li><?=$poke['name']['english']?></li>
+			 <?php } ?>
+		</ol>
+		
+
+			
+		</body>
+	</html>
