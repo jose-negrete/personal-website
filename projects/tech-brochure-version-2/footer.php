7eaf355467dbc9b130cf5d8b3b6c8223e203eb37
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-01-23 16:19:42 -0500

    change to footer and p text in article-intro

--- projects/tech-brochure-version-2/footer.php
+++ projects/tech-brochure-version-2/footer.php
@@ -7,11 +7,11 @@
 		<inner-column>
 
 			<!-- no headers in footers allowed? -->
-			<div class='what-to-call-it-then'>
+<!-- 			<div class='what-to-call-it-then'>
 				<h2 class='attention-voice'>This is the site footer and it has a "site-map" module in it.</h2>
 
 				<p>Pretty standard type of thing, right?</p>
-			</div>
+			</div> -->
 
 			<?php include('modules/site-map.php'); ?>
 
