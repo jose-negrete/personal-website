aed6bbba9e788cbf1e400ae08cddd9091ffacf7b
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-01-23 16:02:26 -0500

    changes to header in article intro

--- projects/tech-brochure-version-2/modules/articles-intro.php
+++ projects/tech-brochure-version-2/modules/articles-intro.php
@@ -2,9 +2,9 @@
 <articles-intro>
 
   <header>
-    <h2 class='attention-voice'>This is and "Article grid" module. This is its heading.</h2>
+    <h2 class='command-voice'>This is and "Article grid" module. This is its heading.</h2>
 
-    <p class='calm-voice'>Again, we totally just made that up. It's a box with boxes in it.</p>
+    <p class='subtle-heading'>Again, we totally just made that up. It's a box with boxes in it.</p>
   </header>
 
   <article-grid>
