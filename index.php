c67310d7b61e460198045d9af6177a9610c2412d
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-02-09 09:50:41 -0500

    renamed index html to php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include("modules/header.php")?>
+		
+		<section class = "welcome">
+			<inner-column>
+			
+			</inner-column>
+		</section>
+
+		<section class = "about">
+			<inner-column>
+			
+			</inner-column>
+		</section>
+		
+		<section class = "writing">
+			<inner-column>
+			
+			</inner-column>
+		</section>
+		
+		<section class = "projects">
+			<inner-column>
+			
+			</inner-column>
+		</section>
+		
+		<section class= "playlist">
+			<inner-column>
+			
+			</inner-column>
+		</section>
+		
+<?php include("modules/footer.php")?>
